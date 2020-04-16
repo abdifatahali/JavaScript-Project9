882509742e28ed41fe204f283117e54bf591fdb8
Abdifatah Ali <48977326+abdifatahali@users.noreply.github.com>
2020-04-16 03:30:46 -0500

    Update co_order.js

--- js/co_order.js
+++ js/co_order.js
@@ -1,17 +1,6 @@
 "use strict";
 
 /*
-   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
-   Tutorial 13
-   Tutorial Case
-
-   Order Form Script
-   
-   Author: Abdifatah Ali
-   Date:   January 27, 2020   
-   
-   Filename: co_order.js
-   
    Function List
    =============
    
